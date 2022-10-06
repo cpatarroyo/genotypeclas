23365ba33e5a16b9ace7f998e6ab740ee8c0708a
cpatarroyo <ca.patarroyo960@hotmail.com>
2022-10-06 16:11:11 -0500

    Tested and added more of the ML infrastructure

--- mlclasificator.R
+++ mlclasificator.R
@@ -5,6 +5,12 @@ library(poppr)
 
 population <- read.genalex("Base de datos ColPeru.csv",ploidy = 3)
 
+
+#bruvo.msn(population, vertex.label = NA)
+population <- clonecorrect(population, strata = NA)
+
+population <- missingno(population, cutoff = 0, type = "geno")
+
 str(population)
 
 population@tab
@@ -13,14 +19,21 @@ population@pop
 restest <- data.frame(Real = population@pop, Pred = NA)
 rownames(restest) <- rownames(population@tab)
 
-head(restest)
+#head(restest)
 
-training <- sort(sample(1:1188,floor(0.7*length(population@pop))))
+training <- sort(sample(1:362,floor(0.7*length(population@pop))))
 
 poptrain <- population@tab[training,]
 poplabel <- as.character(population@pop[training])
+
 poptest <- population@tab[-training,]
+restest <- restest[-training,]
 
 set.seed(999)
 model <- train(poptrain, poplabel, method = 'nb',tuneLength = 10)
 warnings()
+model
+
+restest$Pred <- predict(model,newdata = poptest)
+confusionMatrix.train(model)
+
