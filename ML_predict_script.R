6be8d2cc0d520c6de72a52fb009dc69b7e070989
cpatarroyo <ca.patarroyo960@hotmail.com>
2023-01-25 16:32:25 +0100

    Created 2 scripts, one to train the model and one to predict from new data, to be able to run it in the uniandes computer cluster. These scripts are ready to execute using RScript directly

---
+++ ML_predict_script.R
@@ -0,0 +1,47 @@
+#!/usr/bin/env Rscript
+args = commandArgs(trailingOnly=TRUE)
+
+#Load the required libraries
+library(caret)
+library(poppr)
+library(nnet)
+
+#Define the prediction function
+newPredict <- function(newdata) {
+  #Load the population to be classified
+  newpop <- read.genalex(newdata,ploidy =3)
+  
+  #Load the trained model 
+  trainedModel <- readRDS("clasificator/Trained_model.rds")
+  
+  #Load the allele table 
+  newtable <- newpop@tab
+  
+  #Remove uninformative alleles
+  uninf <- grep("\\.0$",colnames(newtable))
+  newtable <- newtable[,-uninf]
+  
+  #Equate the predictors in the model to the variables in the new dataset
+  missingAl <- which(!trainedModel$finalModel$xNames %in% colnames(newtable))
+  tempMat <- matrix(data = 0, nrow = length(newtable[,1]), ncol = length(missingAl), dimnames = list(rownames(newtable), trainedModel$finalModel$xNames[missingAl]))
+  newtable <- cbind(newtable,tempMat)
+  
+  #Generate the prediction and arrange in a results table
+  resTable <- data.frame(Lineage = predict(trainedModel,newtable), Probability = unlist(apply(predict(trainedModel,newtable,type = "prob"),MARGIN = 1,FUN = max)))
+  
+  #Return the resulting table
+  return(resTable)
+  
+}
+
+# test if there is at least one argument: if not, return an error
+if (length(args)==0) {
+  stop("At least one argument must be supplied (input file).n", call.=FALSE)
+} else if (length(args)==1) {
+  # default output file
+  args[2] = "Prediction.csv"
+}
+
+outputtable <- newPredict(args[1])
+
+write.csv(outputtable, file = args[2])
---
+++ ML_train_script.R
@@ -0,0 +1,32 @@
+library(caret)
+library(poppr)
+library(nnet)
+
+setwd("~/Uniandes/Rfiles/genotypeclas")
+
+poptrain <- read.genalex("Training_DB.csv", ploidy = 3)
+
+modTrain <- function(population) {
+  
+  #Prepare the population table for model training
+  population <- clonecorrect(population, strata = NA)
+  population <- missingno(population, cutoff = 0, type = "geno")
+  poptrain <- population@tab
+  poplabel <- population@pop
+  
+  #Remove uninformative "alleles" 
+  uninf <- grep("\\.0$",colnames(poptrain))
+  poptrain <- poptrain[,-uninf]
+  
+  #Train the neural network model
+  set.seed(999)
+  searchspace <- expand.grid(size =1:8, decay = seq(0,5, by =0.5))
+  trmodel <- train(poptrain,poplabel, method = 'nnet',tuneLength = 1, tuneGrid = searchspace)
+  
+  #Save and return the trained model
+  saveRDS(trmodel, file = "clasificator/Trained_model.rds")
+  print(c((mean(trmodel$resample$Accuracy)),mean(trmodel$resample$Kappa)))
+  #return(trmodel)
+}
+
+modTrain(poptrain)
