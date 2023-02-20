9daf2910862fad95a05fd9753bf2143584f28b64
cpatarroyo <ca.patarroyo960@hotmail.com>
2023-02-20 16:56:13 +0100

    Created files used for testing and for arranging tests of alternative algorithms.

--- NewPopSel.R
+++ NewPopSel.R
@@ -8,3 +8,32 @@ repvec <- c(sample(1:1081,154,replace = FALSE),sample(which(pop(test1eq) == "EU2
 test1eq <- test1eq[repvec]
 
 genind2genalex(test1eq,filename = "Balanced_DS.csv", overwrite = TRUE)
+
+#Function to prepare the population Data for phytophtora ID
+
+prepare <- function(route, index=NULL) {
+  tempTab <- read.csv(route, header=T, skip = 2)
+  mmsInd <- seq(3, length(tempTab[1,]), by=3)
+  
+  ssrTab <- data.frame(tempTab$Ind,"query")
+  popTab <- data.frame(tempTab$Ind,tempTab$Pop)
+  
+  for(i in mmsInd) {
+    ssrTab <- cbind(ssrTab, as.data.frame(paste(tempTab[,i],tempTab[,i+1],tempTab[,i+2],sep = "/")))
+  }
+  
+  if(is.null(index)) {
+    ssrname <- "Tabla_SSR.csv"
+    popname <- "Real_pops.csv"
+  }
+  else {
+    ssrname <- paste("Tabla_SSR",index,".csv",collapse = "_")
+    popname <- paste("Real_pops",index,".csv",collapse = "_")
+  }
+  
+  colnames(ssrTab) <- c("Id","Lineage",colnames(tempTab)[mmsInd])
+  colnames(popTab) <- c("Id", "Lineage")
+  
+  write.csv(ssrTab,file = ssrname, row.names = FALSE)
+  write.csv(popTab,file = popname, row.names = FALSE)
+}
